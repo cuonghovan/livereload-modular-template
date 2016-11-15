f41e2695117df00c216ba4c0871b3049161904cb
Cuong Ho <cuong.ict.10101@gmail.com>
2016-11-15 13:21:20 +0700

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create();
+var less = require('gulp-less');
+
+gulp.task('browser-sync', function() {
+  bs.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+gulp.task('less', function(){
+  gulp.src('style/less/*.less')
+      .pipe(less())
+      .pipe(gulp.dest('style/css/'));
+});
+
+gulp.task('watch', ['browser-sync'], function() {
+  gulp.watch("style/less/*.less", ['less']);
+  gulp.watch("style/css/*.css").on('change', bs.reload);
+  gulp.watch("*.html").on('change', bs.reload);
+  gulp.watch("script/*.js").on('change', bs.reload);
+});
